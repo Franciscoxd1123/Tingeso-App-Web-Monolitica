8467cbefb7103d8812f13da248dd276bff86f3e3
francisco cea <francisco.cea@usach.cl>
2024-10-03 21:13:07 -0300

    Proyecto inicializado desde cero

---
+++ App-Web-Monolitica/src/main/java/com/example/App/Web/Monolitica/AppWebMonoliticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.App.Web.Monolitica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebMonoliticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebMonoliticaApplication.class, args);
+	}
+
+}
---
+++ App-Web-Monolitica/src/test/java/com/example/App/Web/Monolitica/AppWebMonoliticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.App.Web.Monolitica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebMonoliticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
